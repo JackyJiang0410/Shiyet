1b617e9b402d235f1cf31a82754ddf3f8a2ad278
JackyJiang0410 <hj37@rice.edu>
2023-09-22 22:21:02 -0500

    Initial commit

---
+++ Shiyet/Shiyet/ShiyetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShiyetApp.swift
+//  Shiyet
+//
+//  Created by 江昊 on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct ShiyetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
